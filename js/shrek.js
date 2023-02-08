601697b7cca3cc25a0968b6d885e078b2817d7f7
mainblissey24 <mimikyugx100@gmail.com>
2023-02-08 14:19:58 -0300

    alteracoes

---
+++ js/shrek.js
@@ -0,0 +1,12 @@
+function filme(){
+    document.getElementById('conteudo').style.display ='none';
+    document.getElementById('mostrarfilme').style.display ='inline-block';
+    document.getElementById('vertrailer').style.display ='none';
+
+}
+
+function trailer(){
+    document.getElementById('conteudo').style.display ='none';
+    document.getElementById('mostrarfilme').style.display ='none';
+    document.getElementById('vertrailer').style.display ='inline-block';
+}
